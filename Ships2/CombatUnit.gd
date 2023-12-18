022f6dc9714dbab9c21c6e1a9c257a284708f867
Notgnihsaw <52753287+Notgnihsaw@users.noreply.github.com>
2023-12-18 10:43:47 -0500

    major refactor of ship / carrier relationship

--- Ships2/CombatUnit.gd
+++ Ships2/CombatUnit.gd
@@ -4,6 +4,13 @@ class_name CombatUnit
 var unitData: EntityJSON
 
 export var detector_scene: PackedScene
+export var Healthbar: PackedScene
+export var Turret: PackedScene
+
+var faction_visibility_group = "visible_to_"
+
+var healthbar
+var healthbar_offset = Vector2(-15, 30)
 
 # turret type
 var turrets = []
@@ -25,15 +32,34 @@ func getWindResist():
 func getHiding():
 	return self.unitData.get_hiding()
 	
+signal new_course_change(current_target, placement)
+signal reached_target()
+signal ship_lost()
+signal hit(ship)
+
+signal ship_selected(ship)
+signal ship_deselected(ship)
+
+# Combat stuff
+var combat_entity
+var combat_target
+var in_combat = false
+var combat_ticks = 0
 	
 var speed: int
 var base_speed: int
 var turn_speed: int
 var turn_weight: float
 
+# speed, fuel
+var speed_mode_dict = {
+	"stopped": [0, 0],
+	"half": [0.5, 0.2],
+	"full": [1, 0.5],
+	"flank": [1.5, 1]
+}
 
-# stopped, half, full ahead, flank
-var speed_array = [0, speed / 2, speed, int(speed * 1.2)]
+var current_speed_mode
 
 
 var visibility: float
@@ -56,6 +82,12 @@ var placing = false
 var initial_rot = 0
 var screen_size
 
+var stopped = false
+var patrolling = false
+var target_array = []
+var target_angle = {"turning": false, "turn_point": Vector2(0,0)}
+
+
 # ummmm what is the difference between "current speed" and "actual speed"
 var current_speed
 var actual_speed
@@ -70,6 +102,7 @@ var sprite_type
 # GUI variables
 var last_button = ""
 var show_path = true
+
 
 func init(entity, initial_position, faction, type):
 	self.unitData = entity
@@ -111,6 +144,70 @@ func init(entity, initial_position, faction, type):
 	self.applied_wind = Vector2(0, 0)
 	# Set max of health bar and Armor Bar
 	
+	# global setups
+	add_to_group("ship")
+	
+	# add self to proper group (faction)
+	if self.faction == 0:
+		add_to_group("faction_0")
+	elif self.faction == 1:
+		add_to_group("faction_1")
+	elif self.faction == 2:
+		add_to_group("faction_2")
+		
+	self.faction_visibility_group += str(faction)
+	add_to_group(faction_visibility_group)
+		
+	if self.faction != GameState.get_playerFaction():
+		add_to_group("enemy")
+	else:
+		add_to_group("player")
+		
+	# a ship should have three groups (flags): ship, faction, animosity
+	
+	# Combat Variables:
+	get_node("ShotTimer").wait_time = GameState.get_combatPace()
+	
+	self.scale = Vector2(1.2, 1.2)
+	
+	screen_size = get_viewport_rect().size
+	
+	last_button = ""
+	
+	self.deselect()
+	
+	stop_moving()
+	#print("stopped with the ready method, starting game.")
+	
+	# create turrets here:
+	for w in self.get_weapon_list():
+		var turret = Turret.instance()
+		
+		turret.init(w, faction)
+		
+		add_child(turret)
+		
+		turrets.append(turret)
+	
+	setup_specific_unit()
+	
+func _ready():
+	# this has to be called here, cause we need a root scene
+	
+	# Healthbar
+	healthbar = Healthbar.instance()
+	get_tree().root.add_child(healthbar)
+	healthbar.value = self.get_health()
+	healthbar.max_value = self.get_health()
+	
+	if self.faction != GameState.get_playerFaction():
+		healthbar.visible = false
+		
+	get_node("ShotTimer").start()
+	
+func setup_specific_unit():
+	pass
+	
 #creates new velocity vector with applied wind
 func calc_new_velocity():
 	# if we don't add the PI/2 here, it will fuck it all up lmao
@@ -144,14 +241,6 @@ func set_path_showing(new_showing):
 func get_path_showing():
 	return self.show_path
 	
-
-
-# SELECT / DESELCT
-func select():
-	pass
-
-func deselect():
-	pass
 
 
 	
@@ -218,8 +307,104 @@ func stop_placing():
 	print("stopped placing ship")
 
 
+## MOVEMENT
+func handle_final_turn(turn_point):
+	self.target_angle["turning"] = true
+	
+	self.target_angle["turn_point"] = turn_point
 
+func handle_right_mouse_movement(placement):
+### Ship things:
+		if Input.is_action_pressed("queue"):
+			target_array.append(placement)
+			
+			emit_signal("new_course_change", current_target, placement)
+			#print(target_array)
+			
+		elif last_button == "patrol":
+			print("patrolling")
+			patrolling = true
+			current_target = placement
+			target_array.append(self.global_position)
+			
+			#print(target_array)
+			#print(current_target)
+			#print(self.global_position)
+			#print("patrolling value:", patrolling)
+			
+			emit_signal("new_course_change", current_target, placement)
+			
+			last_button = ""
+			
+			# need to start moving when you begin a patrol...
+			if stopped:
+				start_moving()
+			
+		else:
+			patrolling = false
+			target_array = []
+			#print("stopped patrolling for some reason?")
 
+			# unstops the ship, and also sets the current speed mode
+			if stopped:
+				start_moving()
+				#end_repairs()
+				
+				# flank or half from stop
+				if last_button == "flank" || last_button == "half":
+					set_current_speed_mode(last_button)
+					calc_current_speed()
+	
+				
+			# handles setting a current target while already flanking 
+			elif get_current_speed_mode() == "flank" || get_current_speed_mode() == "half":
+				set_current_speed_mode(get_current_speed_mode())
+			
+			current_target = placement
+			
+
+func handle_ship_inputs():
+	if Input.is_action_pressed("patrol"):
+		last_button = "patrol"
+		
+	elif Input.is_action_pressed("flank speed"):
+		last_button = "flank"
+		
+		# set speed to be higher here
+		set_current_speed_mode("flank")
+		calc_current_speed()
+	
+	elif Input.is_action_pressed("half speed"):
+		last_button = "half"
+		
+		# set speed to be half here
+		set_current_speed_mode("half")
+		calc_current_speed()
+	
+	elif Input.is_action_pressed("full ahead"):
+		last_button = "full"
+		
+		# set speed to be full here
+		set_current_speed_mode("full")
+		calc_current_speed()
+	
+	elif Input.is_action_pressed("stop"):
+
+		set_current_speed_mode("stopped")
+		calc_current_speed()
+		self.current_target = self.global_position
+		self.target_array = []
+		
+	
+	## COMBAT
+	elif Input.is_action_just_pressed("shoot"):
+		#print("shooting turrets")
+		if selected and combat_enabled:
+			self.shoot_turrets()
+	
+	
+	elif Input.is_action_pressed("cancel"):
+		last_button = ""
 
 
 func enable_spotting():
@@ -237,10 +422,6 @@ func get_armor():
 	
 	
 ## COMBAT
-func align_turrets():
-	
-	pass
-	
 func unlock_turrets():
 	for t in turrets:
 		t.unlock()
@@ -257,9 +438,6 @@ func shoot_turrets():
 	
 func get_weapon_list():
 	return unitData.get_weapons()
-
-func _ready():
-	pass
 	
 func enable_combat():
 	self.combat_enabled = true
@@ -269,6 +447,125 @@ func disable_combat():
 
 func is_plane():
 	return false
+	
+func take_damage(weapon: Weapon):
+	
+	self.unitData.damage(weapon)
+	
+	healthbar.value = self.get_health()
+	
+	if self.get_health() <= 0:
+		print("ship lost")
+		emit_signal("ship_lost", self)
+		
+		healthbar.queue_free()
+	
+func update_healthbar():
+	if self.is_in_group("visible_to_" + str(GameState.get_playerFaction())):
+		healthbar.visible = true
+		healthbar.set_global_position(self.global_position + healthbar_offset)
+		healthbar.set_rotation(0)
+	else:
+		healthbar.visible = false
+	
+func scan_detection_radius():
+	
+	for body in detector.get_overlapping_bodies():
+		if body != self:
+			body.call("detect")
+	
+func set_firing_target(firing_target):
+	
+	# idk how to do this one yet
+	self.firing_target = firing_target
+
+# update this later, once turrets are added
+func _on_ShotTimer_timeout():
+	if is_instance_valid(combat_entity) and in_combat:
+		self.shoot_ship_turrets(combat_ticks)
+		
+		self.combat_ticks += 1
+		
+func shoot_ship_turrets(combat_ticks):
+	
+	for t in turrets:
+
+		if int(combat_ticks) % int(t.get_fire_rate()) == 0:
+			
+			if t.is_aa_gun() and combat_entity.is_plane():
+				t.shoot()
+			elif not t.is_aa_gun() and not combat_entity.is_plane():
+				t.shoot()
+
+# if / when we add back fuel, we can use the prototypes in the Ship Squadron class.
+
+func set_current_speed_mode(speed_mode):
+	current_speed_mode = speed_mode
+
+func get_current_speed_mode():
+	return current_speed_mode
+
+func calc_current_speed():
+	
+	#if out_of_fuel():
+		# this will automatically refuel the ship. 
+		# the player will have to manually stop repairs to continue.
+	#	start_repairs()
+
+	current_speed = speed_mode_dict[current_speed_mode][0] * self.getSpeed()
+
+
+
+
+## COMBAT
+# this is unique to the ships – different for planes
+# bugged for now 
+func align_turrets():
+	
+	# we do this for each turret so they can independently target things.
+	# change later?
+	var valid_enemies = 0
+	
+	for t in turrets:
+		var all_enemy = get_tree().get_nodes_in_group(faction_visibility_group)
+		
+		for enemy in all_enemy:
+			var gun2enemy_distance = self.global_position.distance_to(enemy.global_position)
+			
+			#print(gun2enemy_distance)
+			if gun2enemy_distance < t.weaponData.get_range() and \
+				enemy.get_faction() != self.faction:
+				# this is basically an XOR: the aa gun and plane have to match up
+				if enemy.is_plane() and not t.is_aa_gun():
+					pass
+					
+				else:
+					combat_entity = enemy  ## --->## after get the current close_enemy
+					
+					# lerped (slowed down rotation)
+					# need to use global rotation otherwise things get bad
+					combat_target = combat_entity.global_position
+					
+					# t here is turret
+					t.global_rotation = lerp_angle(t.global_rotation, 
+						(combat_target - t.global_position).normalized().angle(), 
+						t.turn_weight)
+						
+					valid_enemies += 1
+				
+	if self.in_combat == false and valid_enemies > 0:
+		self.enter_combat()
+	
+	if valid_enemies == 0:
+		self.exit_combat()
+				
+
+func enter_combat():
+	self.in_combat = true
+	
+func exit_combat():
+	self.in_combat = false
+
 
 # Detection:
 func detect():
@@ -279,3 +576,140 @@ func un_detect():
 	if self.faction != GameState.get_playerFaction():
 		self.hide()
 
+
+## COMMON FUNCS
+func start_moving():
+	#print("started moving")
+	
+	stopped = false
+	set_current_speed_mode("full")
+	calc_current_speed()
+
+func stop_moving():
+	stopped = true
+	set_current_speed_mode("stopped")
+
+
+## Selection:
+func select():
+	
+	if self.is_in_group("player"):
+		
+		selected = true
+		get_node("Sprite").animation = type + "_clicked"
+		get_node("Sprite").set_frame(faction)
+		
+		#print(get_node("Sprite").animation)
+		
+		#print("carrier selected")
+		# yikes this might not hold up
+		emit_signal("ship_selected", self)
+		
+		last_button = ""
+		
+		show_ghost_sprite()
+		
+func deselect():
+	selected = false
+	
+	get_node("Sprite").animation = type + "_basic"
+	get_node("Sprite").set_frame(faction)
+	
+	#print(get_node("Sprite").animation)
+	#print("carrier deselected")
+	
+	emit_signal("ship_deselected", self)
+	
+	hide_ghost_sprite()
+	
+	handle_specific_deselect()
+	
+	#get_node("ScoutPlaneTriangle").visible = false
+
+func handle_specific_deselect():
+	pass
+
+## PHYSICS:
+func _physics_process(delta):
+	if placing:
+		# Place squadron at mouse, until the mouse clicks
+		global_position = get_viewport().get_mouse_position()
+		current_target = self.global_position
+	
+	else:
+		
+		var movement_vector = Vector2(0,0)
+		#if distance to target is small and there are queued targets,
+		#remove the target from the queue and update the current target
+		
+		
+		#print("patrolling, target array:", target_array)
+		if global_position.distance_to(current_target) < 10 and len(target_array) > 0:
+			emit_signal("reached_target")
+			
+			if patrolling: 
+				target_array.append(current_target)
+				#print("patrolling, target array:", target_array)
+			
+			current_target = target_array[0]
+			
+			target_array.remove(0)
+		
+		#if we are close to the last target, set stopped to true
+		elif global_position.distance_to(current_target) < 10:
+			
+			emit_signal("reached_target")
+			
+			stop_moving()
+
+		if int(global_position.distance_to(current_target)) > 1 and not stopped:
+			self.rotation = lerp_angle(self.rotation, (current_target - self.global_position).normalized().angle() + PI/2, self.turn_weight)
+
+		if not stopped:
+			self.calc_new_velocity()
+			
+			velocity_vector = move_and_slide(self.velocity_vector)
+			
+		elif self.target_angle["turning"]:
+			var angle_to_turn_target = (target_angle["turn_point"] - self.global_position).normalized().angle() + PI/2
+			
+			self.global_rotation = lerp_angle(self.global_rotation, angle_to_turn_target, self.turn_weight)
+			# recycling some code earlier – couldn't figure it out any other way
+			
+			if abs(angle_to_turn_target - self.global_rotation) <= 0.01:
+				# if we reached the turn targe,t stope turning
+				self.target_angle["turning"] = false
+				self.target_angle["turn_point"] = self.global_position
+
+func draw_ghost_sprite():
+	# here, we put the position of the ghost sprite over the mouse
+	# then, if the right mouse is already being held down, 
+	# we rotate the sprite so it's facing the right mouse
+	
+	var ghost_sprite = get_node("GhostSprite")
+	
+	# sets the alpha value to be very low
+	ghost_sprite.self_modulate.a = 0.2
+	
+	ghost_sprite.global_position = get_global_mouse_position()
+	
+	if Input.is_action_pressed("right_click"):
+		#print("rotating ghost sprite")
+		# ideally, this should stick on the temp target
+		ghost_sprite.global_position = self.temp_target
+		ghost_sprite.look_at(get_global_mouse_position())
+		ghost_sprite.rotation += PI/2
+
+
+func hide_ghost_sprite():
+	#print("hiding ghost sprite")
+	get_node("GhostSprite").visible = false
+
+func show_ghost_sprite():
+	get_node("GhostSprite").visible = true
+	
+func set_temp_target(new_temp_target):
+	self.temp_target = new_temp_target
+
+func get_temp_target():
+	return self.temp_target
--- Ships2/Plane Launchers/Carrier.gd
+++ Ships2/Plane Launchers/Carrier.gd
@@ -4,21 +4,7 @@ class_name CarrierScene
 # Ship Scene stuff
 func get_class(): return "Carrier"
 
-export var Turret: PackedScene
 export var TorpedoTube: PackedScene
-
-signal new_course_change(current_target, placement)
-signal reached_target()
-signal ship_lost()
-signal hit(ship)
-
-signal ship_selected(ship)
-signal ship_deselected(ship)
-
-var stopped = false
-var patrolling = false
-var target_array = []
-var target_angle = {"turning": false, "turn_point": Vector2(0,0)}
 
 # this is for placing the ghost sprite
 var temp_target = Vector2(0,0)
@@ -31,16 +17,6 @@ var burning_animation = false
 
 # repairing
 var repairing = false
-
-# speed, fuel
-var speed_mode_dict = {
-	"stopped": [0, 0],
-	"half": [0.5, 0.2],
-	"full": [1, 0.5],
-	"flank": [1.5, 1]
-}
-
-var current_speed_mode
 
 
 ### Airbase Things:
@@ -66,55 +42,17 @@ var combat_air_patrol = false
 
 var plane_numbers: Dictionary
 
-
 func _ready():
-
-	add_to_group("ship")
+	pass
+	
+func setup_specific_unit():
+	
 	add_to_group("carrier")
-	
-	# add self to proper group (faction)
-	if self.faction == 0:
-		add_to_group("faction_0")
-	elif self.faction == 1:
-		add_to_group("faction_1")
-	elif self.faction == 2:
-		add_to_group("faction_2")
-		
-	if self.faction != GameState.get_playerFaction():
-		add_to_group("enemy")
-	else:
-		add_to_group("player")
-		
-	# a ship should have three groups (flags): ship, faction, animosity
-	
-	# Combat Variables:
-	get_node("ShotTimer").wait_time = GameState.get_combatPace()
-	
 	self.scale = Vector2(1.2, 1.2)
-	
-	screen_size = get_viewport_rect().size
-	
-	last_button = ""
-	
-	self.deselect()
-	
-	stop_moving()
-	#print("stopped with the ready method, starting game.")
-	
-	# create turrets here:
-	for w in self.get_weapon_list():
-		var turret = Turret.instance()
-		
-		turret.init(w)
-		
-		add_child(turret)
-		
-		turrets.append(turret)
-	
-	print(self.unitData)
-	print(self.unitData.get_planes())
+	#print(self.unitData)
+	#print(self.unitData.get_planes())
 	self.plane_numbers = self.unitData.get_planes()
-	print(self.plane_numbers)
+	#print(self.plane_numbers)
 	
 	get_node("ScoutPlaneTriangle").visible = false
 	get_node("ScoutPlaneTriangle").color = Color(0.2, 0.5, 0.3, 0.3)
@@ -128,6 +66,9 @@ func _ready():
 	fighter_targets = get_node("FighterPatrolCircle").polygon
 	get_node("FighterPatrolCircle").scale = Vector2(0.8, 0.8)
 	
+	# setup healthbar
+	healthbar_offset = Vector2(-15, 50)
+
 func handle_right_click(placement):
 	#print("handling right click")
 	
@@ -167,70 +108,16 @@ func handle_right_click(placement):
 			self.handle_right_mouse_movement(placement)
 			self.combat_air_patrol = false
 
-func handle_right_mouse_movement(placement):
-### Ship things:
-		if Input.is_action_pressed("queue"):
-			target_array.append(placement)
-			
-			emit_signal("new_course_change", current_target, placement)
-			#print(target_array)
-			
-		elif last_button == "patrol":
-			print("patrolling")
-			patrolling = true
-			current_target = placement
-			target_array.append(self.global_position)
-			
-			#print(target_array)
-			#print(current_target)
-			#print(self.global_position)
-			#print("patrolling value:", patrolling)
-			
-			emit_signal("new_course_change", current_target, placement)
-			
-			last_button = ""
-			
-			# need to start moving when you begin a patrol...
-			if stopped:
-				start_moving()
-			
-		else:
-			patrolling = false
-			target_array = []
-			#print("stopped patrolling for some reason?")
-
-			# unstops the ship, and also sets the current speed mode
-			if stopped:
-				start_moving()
-				#end_repairs()
-				
-				# flank or half from stop
-				if last_button == "flank" || last_button == "half":
-					set_current_speed_mode(last_button)
-					calc_current_speed()
-	
-				
-			# handles setting a current target while already flanking 
-			elif get_current_speed_mode() == "flank" || get_current_speed_mode() == "half":
-				set_current_speed_mode(get_current_speed_mode())
-			
-			current_target = placement
-
 func launch_planes(placement, strike_type):
 	
 	self.send_out_planes(placement, strike_type, true)
 	
 	#self.stop_moving()
 
-func handle_final_turn(turn_point):
-	self.target_angle["turning"] = true
-	
-	self.target_angle["turn_point"] = turn_point
-	
-	#print(self.target_angle)
 
 func _input(event):
 	if selected:
+		handle_ship_inputs()
 		
 		## Airbase Things
 		if Input.is_action_pressed("scout"):
@@ -250,89 +137,16 @@ func _input(event):
 			elif Input.is_action_pressed("fighter"):
 				last_button = "CAP"
 		
-		if Input.is_action_pressed("stop"):
-
-			set_current_speed_mode("stopped")
-			calc_current_speed()
-			self.current_target = self.global_position
-			self.target_array = []
-		
 		if Input.is_action_pressed("stop_launch"):
 			self.stop_launching()
-		
-		elif Input.is_action_pressed("patrol"):
-			last_button = "patrol"
-			
-		elif Input.is_action_pressed("flank speed"):
-			last_button = "flank"
-			
-			# set speed to be higher here
-			set_current_speed_mode("flank")
-			calc_current_speed()
-		
-		elif Input.is_action_pressed("half speed"):
-			last_button = "half"
-			
-			# set speed to be half here
-			set_current_speed_mode("half")
-			calc_current_speed()
-		
-		elif Input.is_action_pressed("full ahead"):
-			last_button = "full"
-			
-			# set speed to be full here
-			set_current_speed_mode("full")
-			calc_current_speed()
-			
-		
-		## COMBAT
-		elif Input.is_action_just_pressed("shoot"):
-			#print("shooting turrets")
-			if selected and combat_enabled:
-				self.shoot_turrets()
-		
-		
-		elif Input.is_action_pressed("cancel"):
-			last_button = ""
 
 # change this (hardcode) for now.
 # change arrow to the ship type later
 
-func select():
 	
-	if self.is_in_group("player"):
-		
-		selected = true
-		get_node("CarrierSprite").animation = type + "_clicked"
-		get_node("CarrierSprite").set_frame(faction)
-		
-		#print(get_node("Sprite").animation)
-		
-		#print("carrier selected")
-		# yikes this might not hold up
-		emit_signal("ship_selected", self)
-		
-		last_button = ""
-		
-		unlock_turrets()
-		
-		show_ghost_sprite()
-		
-func deselect():
-	selected = false
-	
-	get_node("CarrierSprite").animation = type + "_basic"
-	get_node("CarrierSprite").set_frame(faction)
-	
-	lock_turrets()
-	
-	#print(get_node("Sprite").animation)
-	#print("carrier deselected")
-	
-	emit_signal("ship_deselected", self)
-	
-	hide_ghost_sprite()
-	
+
+
+func handle_specific_deselect():
 	get_node("ScoutPlaneTriangle").visible = false
 
 # these are here for later, if we build ports n shit
@@ -357,16 +171,6 @@ func deselect():
 	
 # movement functions:
 # took out fuel indicators – idk how to do fuel with the new ships.
-func start_moving():
-	#print("started moving")
-	
-	stopped = false
-	set_current_speed_mode("full")
-	calc_current_speed()
-
-func stop_moving():
-	stopped = true
-	set_current_speed_mode("stopped")
 
 # Handle Island collisions
 func _on_Squadron_area_entered(area):
@@ -388,56 +192,7 @@ func do_fog_effects():
 	
 func _physics_process(delta):
 	
-	if placing:
-		# Place squadron at mouse, until the mouse clicks
-		global_position = get_viewport().get_mouse_position()
-		current_target = self.global_position
-	
-	else:
-		
-		var movement_vector = Vector2(0,0)
-		#if distance to target is small and there are queued targets,
-		#remove the target from the queue and update the current target
-		
-		
-		#print("patrolling, target array:", target_array)
-		if global_position.distance_to(current_target) < 10 and len(target_array) > 0:
-			emit_signal("reached_target")
-			
-			if patrolling: 
-				target_array.append(current_target)
-				#print("patrolling, target array:", target_array)
-			
-			current_target = target_array[0]
-			
-			target_array.remove(0)
-		
-		#if we are close to the last target, set stopped to true
-		elif global_position.distance_to(current_target) < 10:
-			
-			emit_signal("reached_target")
-			
-			stop_moving()
-
-		if int(global_position.distance_to(current_target)) > 1 and not stopped:
-			self.rotation = lerp_angle(self.rotation, (current_target - self.global_position).normalized().angle() + PI/2, self.turn_weight)
-
-		if not stopped:
-			self.calc_new_velocity()
-			
-			velocity_vector = move_and_slide(self.velocity_vector)
-			
-		elif self.target_angle["turning"]:
-			var angle_to_turn_target = (target_angle["turn_point"] - self.global_position).normalized().angle() + PI/2
-			
-			self.global_rotation = lerp_angle(self.global_rotation, angle_to_turn_target, self.turn_weight)
-			# recycling some code earlier – couldn't figure it out any other way
-			
-			if abs(angle_to_turn_target - self.global_rotation) <= 0.01:
-				# if we reached the turn targe,t stope turning
-				self.target_angle["turning"] = false
-				self.target_angle["turn_point"] = self.global_position
-	
+	pass
 	#position.x = clamp(position.x, 0, screen_size.x)
 	#position.y = clamp(position.y, 0, screen_size.y)
 
@@ -453,44 +208,15 @@ func _process(delta):
 	scout_targets.remove(0)
 	
 	fighter_targets = get_node("FighterPatrolCircle").polygon
-
 	
-func set_firing_target(firing_target):
+	update_healthbar()
 	
-	# idk how to do this one yet
-	self.firing_target = firing_target
-
-# update this later, once turrets are added
-func _on_ShotTimer_timeout():
+	## find overlapping bodies to spot
+	scan_detection_radius()
 	
-	pass
+	align_turrets()
 	
-# TODO: make a bullet deal damage.
-func take_damage(weapon: Weapon):
-	
-	self.unitData.damage(weapon)
-	
-	if self.get_health() <= 0:
-		print("ship lost")
-		emit_signal("ship_lost", self)
-
 # if / when we add back fuel, we can use the prototypes in the Ship Squadron class.
-
-func set_current_speed_mode(speed_mode):
-	current_speed_mode = speed_mode
-
-func get_current_speed_mode():
-	return current_speed_mode
-
-func calc_current_speed():
-	
-	#if out_of_fuel():
-		# this will automatically refuel the ship. 
-		# the player will have to manually stop repairs to continue.
-	#	start_repairs()
-
-	current_speed = speed_mode_dict[current_speed_mode][0] * self.getSpeed()
-
 
 
 
@@ -503,39 +229,6 @@ func calc_current_speed():
 #
 #	for t in turrets:
 #		t.point_to(mouse_position)
-
-
-func draw_ghost_sprite():
-	# here, we put the position of the ghost sprite over the mouse
-	# then, if the right mouse is already being held down, 
-	# we rotate the sprite so it's facing the right mouse
-	
-	var ghost_sprite = get_node("GhostSprite")
-	
-	# sets the alpha value to be very low
-	ghost_sprite.self_modulate.a = 0.2
-	
-	ghost_sprite.global_position = get_global_mouse_position()
-	
-	if Input.is_action_pressed("right_click"):
-		#print("rotating ghost sprite")
-		# ideally, this should stick on the temp target
-		ghost_sprite.global_position = self.temp_target
-		ghost_sprite.look_at(get_global_mouse_position())
-		ghost_sprite.rotation += PI/2
-	
-func hide_ghost_sprite():
-	#print("hiding ghost sprite")
-	get_node("GhostSprite").visible = false
-
-func show_ghost_sprite():
-	get_node("GhostSprite").visible = true
-
-func set_temp_target(new_temp_target):
-	self.temp_target = new_temp_target
-
-func get_temp_target():
-	return self.temp_target
 
 
 ## PLANE thingS:
